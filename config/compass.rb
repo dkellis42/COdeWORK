76e2ef17d9d6d1a92acfff2083e675e057ef7927
Emma Follender <efollender@gmail.com>
2014-09-22 16:00:18 -0500

    Moved stylesheets directory outside of lib

--- config/compass.rb
+++ config/compass.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/lib/stylesheets"
+css_dir = "public/stylesheets"
 sass_dir = "public/lib/bootstrap"
 images_dir = "public/lib/images"
 javascripts_dir = "javascripts"
