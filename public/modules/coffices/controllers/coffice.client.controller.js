9c4614f28c5f91660f4148412a8951d794917162
Emma Follender <efollender@gmail.com>
2014-09-23 17:27:32 -0500

    Fixed css for search results

--- public/modules/coffices/controllers/coffice.client.controller.js
+++ public/modules/coffices/controllers/coffice.client.controller.js
@@ -9,7 +9,7 @@ angular.module('coffices').controller('CofficeController', ['$scope', 'distance'
     $scope.query = "coffee,wifi";
     $scope.testCoffices  = {};
     $scope.lookup = function(){ 
-      var foursquareQuery = $http.get("https://api.foursquare.com/v2/venues/explore?client_id=" + $scope.clientID + "&client_secret=" + $scope.clientSecret + "&sortByDistance=1&venuePhotos=1&v=20140910&near=" + $scope.near +"&query=" + $scope.query);
+      var foursquareQuery = $http.get("https://api.foursquare.com/v2/venues/explore?client_id=" + $scope.clientID + "&client_secret=" + $scope.clientSecret + "&venuePhotos=1&v=20140910&near=" + $scope.near +"&query=" + $scope.query);
       foursquareQuery.success(function(data, status, headers, config) {
            $scope.testCoffices.list = data.response.groups[0].items;
            console.log('data',$scope.testCoffices.list);
