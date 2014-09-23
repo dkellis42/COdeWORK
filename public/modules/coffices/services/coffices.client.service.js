b4ec7775c53278de0f85d081e72b64e660f0b936
Emma Follender <efollender@gmail.com>
2014-09-23 16:50:09 -0500

    Added distance. Thanks, Foursquare. Lol.

--- public/modules/coffices/services/coffices.client.service.js
+++ public/modules/coffices/services/coffices.client.service.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('coffices')
-  .service('cofficeLookup', [
+  .service('distance', [
     function(){
-      this.getCoffices = function(client_id, client_secret, near, query){
-        "https://api.foursquare.com/v2/venues/explore?client_id=" + client_id + "&client_secret=" + client_secret + "&v=20140910&near=" + near +"&query=" + query;
+      this.rad = function(x) { return x * Math.PI / 180 };
+
+      this.haversine = function(p1, p2) {
+        var R = 6371;
+        var dLat  = this.rad(p2.lat - p1.lat);
+        var dLong = this.rad(p2.lng - p1.lng);
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.rad(p1.lat)) * Math.cos(this.rad(p2.lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+
+        return Math.round(d);
+      };
+      this.toMiles = function(km){
+        return Math.round(km * 0.621371 * 100)/100;
+      };
+      this.getDistance = function(coffice,near) {
+        var d = this.haversine(coffice.venue.location, {lat: 30.2463, lng:  -97.7609});
+        return this.toMiles(d);
       };
     }
   ]);
