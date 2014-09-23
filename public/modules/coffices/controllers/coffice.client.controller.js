6a78982b08c21496f7a7e94eb78939cf6a2998de
Emma Follender <efollender@gmail.com>
2014-09-23 15:45:50 -0500

    Got photos working. :( DEREK IS A POOP

--- public/modules/coffices/controllers/coffice.client.controller.js
+++ public/modules/coffices/controllers/coffice.client.controller.js
@@ -9,11 +9,16 @@ angular.module('coffices').controller('CofficeController', ['$scope', 'cofficeLo
     $scope.query = "coffee,wifi";
     $scope.testCoffices  = {};
     $scope.lookup = function(){ 
-      var foursquareQuery = $http.get("https://api.foursquare.com/v2/venues/explore?client_id=" + $scope.clientID + "&client_secret=" + $scope.clientSecret + "&v=20140910&near=" + $scope.near +"&query=" + $scope.query);
+      var foursquareQuery = $http.get("https://api.foursquare.com/v2/venues/explore?client_id=" + $scope.clientID + "&client_secret=" + $scope.clientSecret + "&sortByDistance=1&venuePhotos=1&v=20140910&near=" + $scope.near +"&query=" + $scope.query);
       foursquareQuery.success(function(data, status, headers, config) {
            $scope.testCoffices.list = data.response.groups[0].items;
            console.log('data',$scope.testCoffices.list);
       });
+    };
+    $scope.getCofficePhoto = function(coffice, size){
+      var cPrefix = coffice.venue.photos.groups[0].items[0].prefix;
+      var cSuffix = coffice.venue.photos.groups[0].items[0].suffix;
+      return cPrefix + size +cSuffix;
     };
   }
 ]);
