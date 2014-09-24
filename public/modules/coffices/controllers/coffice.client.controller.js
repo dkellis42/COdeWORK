f67480063f70123b3ae40282f87302d99da3be6e
Derek Ellis <dkellis42@gmail.com>
2014-09-24 11:00:56 -0500

    a

--- public/modules/coffices/controllers/coffice.client.controller.js
+++ public/modules/coffices/controllers/coffice.client.controller.js
@@ -6,7 +6,7 @@ angular.module('coffices').controller('CofficeController', ['$scope', 'distance'
     $scope.clientID = "03YGRUTGE1CNGSV5BZA2JFMUCKZBJEP1YKHPOEGYSRTGU2VG";
     $scope.clientSecret = "15ULA34FN42K3XKHORE4K2CU0Y4CHBHSAIHJ1G01QRPG5Z1H";
     $scope.near = "78704";
-    $scope.query = "coffee,wifi,mb";
+    $scope.query = "coffee,wifi";
     $scope.testCoffices  = {'list':[]};
     $scope.lookup = function(clientID, clientSecret, near, query ){ 
       var foursquareQuery = $http.get("https://api.foursquare.com/v2/venues/explore?client_id=" + $scope.clientID + "&client_secret=" + $scope.clientSecret + "&venuePhotos=1&v=20140910&near=" + $scope.near +"&query=" + $scope.query);
