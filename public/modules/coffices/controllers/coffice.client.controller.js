18d17d7feaf9e3554e2ef51e07e7672d7369e2be
Emma Follender <efollender@gmail.com>
2014-09-24 10:17:08 -0500

    Added ngFitText. Fixed header so that it show s route name and doesn't break. Changed 4sq API request so that it only returns locations with photos.

--- public/modules/coffices/controllers/coffice.client.controller.js
+++ public/modules/coffices/controllers/coffice.client.controller.js
@@ -6,12 +6,17 @@ angular.module('coffices').controller('CofficeController', ['$scope', 'distance'
     $scope.clientID = "03YGRUTGE1CNGSV5BZA2JFMUCKZBJEP1YKHPOEGYSRTGU2VG";
     $scope.clientSecret = "15ULA34FN42K3XKHORE4K2CU0Y4CHBHSAIHJ1G01QRPG5Z1H";
     $scope.near = "78704";
-    $scope.query = "coffee,wifi";
-    $scope.testCoffices  = {};
-    $scope.lookup = function(){ 
+    $scope.query = "coffee,wifi,mb";
+    $scope.testCoffices  = {'list':[]};
+    $scope.lookup = function(clientID, clientSecret, near, query ){ 
       var foursquareQuery = $http.get("https://api.foursquare.com/v2/venues/explore?client_id=" + $scope.clientID + "&client_secret=" + $scope.clientSecret + "&venuePhotos=1&v=20140910&near=" + $scope.near +"&query=" + $scope.query);
       foursquareQuery.success(function(data, status, headers, config) {
-           $scope.testCoffices.list = data.response.groups[0].items;
+          var returnedData = data.response.groups[0].items;
+          for(var i in returnedData){
+            if (returnedData[i].venue.photos.count > 0 ){
+              $scope.testCoffices.list.push(returnedData[i]);
+            }
+          }
            console.log('data',$scope.testCoffices.list);
       });
     };
